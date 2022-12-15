5aa8a84593cf6ed4480828611e14b5061e5a0a40
KanoplichAndrey <kanop0000@gmail.com>
2022-12-15 13:59:09 +0300

    correct

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import '../src/scss/main.scss';
+import '../src/font-awesome/css/font-awesome.min.css'
+// import './index.html';
+// import bmw from './src/img/bmw.jpg'
+import { nameValid } from './nameValid';
+import { submit } from './submit';
+import { sendData } from './sendData';
+
+nameValid;
+window.addEventListener('load', function () {
+  sendData;
+  submit;
+});
---
+++ src/sendData.js
@@ -0,0 +1,25 @@
+// import { Resolver } from 'webpack';
+import { form } from './nameValid'
+
+     
+
+export  function sendData() {
+    
+    const XHR = new XMLHttpRequest();
+    const FD = new FormData( form );
+
+    
+    XHR.addEventListener( "load", function(event) {
+    alert('вашы данные успешно отправлены'),     
+         form.innerHTML = '<h2>спасибо за заявку!</h2>'
+         form.style.color="red";
+    
+    } );
+    
+    XHR.addEventListener( "error", function( event ) {
+    alert( 'Oops! Something went wrong.' );
+    } );
+    
+    XHR.open( "POST", "https://httpbin.org/post" );
+    XHR.send( FD );
+    }
